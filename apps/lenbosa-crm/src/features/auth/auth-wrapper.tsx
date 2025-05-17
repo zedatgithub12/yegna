ffb2cde5ed10219dee8e7fae3934ed4cf6ef35a3
zerihun.tegenu <zerihun.tegenu@eaglelionsystems.com>
2025-05-17 20:02:29 +0300

    merged with dev

--- apps/lenbosa-crm/src/features/auth/auth-wrapper.tsx
+++ apps/lenbosa-crm/src/features/auth/auth-wrapper.tsx
@@ -10,7 +10,7 @@ const AuthWrapper: React.FC<Props> = ({ children }) => {
     <div className="flex h-screen items-center justify-center ">
       <div className="mx-auto p-6 grid w-full h-full grid-cols-1  gap-8 bg-[#F9F7F2] dark:bg-black md:grid-cols-2">
         <div
-          className="mx-auto w-full max-w-xl flex-grow items-center shadow-xl shadow-gray-100 rounded-[30px]"
+          className="hidden md:flex mx-auto w-full max-w-xl flex-grow items-center shadow-xl shadow-gray-100 rounded-[30px]"
           style={{
             backgroundImage: `url(${AuthCover.src})`,
             backgroundSize: "cover",
